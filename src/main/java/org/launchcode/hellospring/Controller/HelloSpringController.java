3cfdc6be0791a4faaf5a6c6ed810e839947c0646
C. Gremaud <colyn.gremaud@gmail.com>
2021-04-15 09:25:36 -0500

    having issue with getting conditionals to work inside controllers. It will receive the params french and name and I can return them and they will display correctly, but if language == french won't trigger the conditional.

--- src/main/java/org/launchcode/hellospring/Controller/HelloSpringController.java
+++ src/main/java/org/launchcode/hellospring/Controller/HelloSpringController.java
@@ -3,8 +3,49 @@ package org.launchcode.hellospring.Controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.Locale;
+
 @Controller
+@ResponseBody
+
+
 public class HelloSpringController {
+
+    @RequestMapping(method = {RequestMethod.GET, RequestMethod.PUT})
+    public String form(){
+        return "<html>" +
+                "<body>" +
+                "<form method = 'post' action = 'results'>" +
+                "<input type='text' name='name' />" +
+                "<select name = 'language' id = 'language'>" +
+                "<option value = \"french\">French</option>" +
+                "<option value = 'english'>English</option>" +
+                "</select>" +
+                "<button>Submit</button>" +
+                "</form>" +
+                "</body>" +
+                "</html>";
+    }
+
+    @PostMapping("results")
+    public String results(@RequestParam String name, @RequestParam String language){
+        String aName = name;
+        String aLanguage = language;
+
+        return createMessage(aName, aLanguage);
+
+//        return name + language;
+    }
+
+    public static String createMessage(String name, String language){
+        if (language == "french") {
+            return "Bonjour " + name;
+        } else if (language == "english") {
+            return "Hello, " + name;
+        } else {
+            return "oops";
+        }
+    }
 
 
 }
